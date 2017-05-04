3c6c4782a55c59f43e0beeb781bbbd9181ce1a68
Nicolas Montes <nicolas.montes.perso@gmail.com>
2017-05-04 09:55:22 +0200

    Remove (1) files and start tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,41 +8,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get users_url
     assert_response :success
+    assert_select "title", "BarCVVR"
   end
 
-  test "should get new" do
-    get new_user_url
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { alias: @user.alias, amount: @user.amount, email: @user.email, firstName: @user.firstName, initAmount: @user.initAmount, lastName: @user.lastName, password_digest: @user.password_digest } }
-    end
-
-    assert_redirected_to user_url(User.last)
-  end
-
-  test "should show user" do
-    get user_url(@user)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: { alias: @user.alias, amount: @user.amount, email: @user.email, firstName: @user.firstName, initAmount: @user.initAmount, lastName: @user.lastName, password_digest: @user.password_digest } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
-    end
-
-    assert_redirected_to users_url
-  end
 end
