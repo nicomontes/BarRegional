1b0dc772595a7ae74817ca0f7ae513b128a83c83
Nicolas Montes <nicolas.montes.perso@gmail.com>
2017-05-07 22:20:55 +0200

    test user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :firstName,  presence: true
+  validates :lastName, presence: true
+  validates :initAmount, numericality: true
+  validates :amount, numericality: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,64 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "firstName should be present" do
+    @user.firstName = ""
+    assert_not @user.valid?
+  end
+  
+  test "lastName should be present and are string" do
+    @user.lastName = ""
+    assert_not @user.valid?
+  end
+  
+  test "initAmount should be present ans numeric" do
+    @user.initAmount = ""
+    assert_not @user.valid?
+    @user.initAmount = "jsdhs"
+    assert_not @user.valid?
+    @user.initAmount = 90
+    assert @user.valid?
+    @user.initAmount = 90.98
+    assert @user.valid?
+    @user.initAmount = -90
+    assert @user.valid?
+    @user.initAmount = -90.98
+    assert @user.valid?
+  end
+  
+  test "amount should be present and numeric" do
+    @user.amount = ""
+    assert_not @user.valid?
+    @user.amount = "qjkshd"
+    assert_not @user.valid?
+    @user.amount = 90
+    assert @user.valid?
+    @user.amount = 90.98
+    assert @user.valid?
+    @user.amount = -90
+    assert @user.valid?
+    @user.amount = -90.98
+    assert @user.valid?
+  end
+  
+  test "email should be present and are email" do
+    @user.email = ""
+    assert_not @user.valid?
+    @user.email = "qsjdkf"
+    assert_not @user.valid?
+    @user.email = 98
+    assert_not @user.valid?
+    @user.email = -87
+    assert_not @user.valid?
+    @user.email = "john.doe+glider@gmail.com"
+    assert @user.valid?
+  end
 end
